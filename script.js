d6097d21cee0818dd32a9bf9947ed93d3d330ccf
leoreyes07 <leoreyes_07@outlook.com>
2025-12-11 22:50:22 -0600

    added JS file and added the modal

---
+++ script.js
@@ -0,0 +1,13 @@
+const modal = document.querySelector('.modal')
+const button1 = document.querySelector('#button1')
+const closeButton = document.querySelector('.modal__container--close')
+
+button1.addEventListener('click', () => {
+    modal.classList.remove('hidden')
+    modal.classList.add('visible')
+})
+
+closeButton.addEventListener('click', () => {
+    modal.classList.add('hidden')
+    modal.classList.remove('visible')
+})
